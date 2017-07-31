4dca6aabccce02bf9cda61a66be60edd31f892e9
Dmitry Chekanov <chekanov.d@gmail.com>
2017-07-31 15:51:27 +0300

    Array sorting

---
+++ t1.pl
@@ -0,0 +1,62 @@
+#! /usr/bin/perl
+
+=head1 DESCRIPTION
+
+		Дан массив чисел. Требуется вывести через пробел числа без повторений, отсортированный по возрастанию.
+
+=head1 AUTHOR
+
+		Dmitriy Chekanov		chekanov.d@gmail.com
+
+=cut
+
+use strict;
+
+my @array = getArray(10,50);
+@array = shellSort(@array);
+showUnique(@array);
+
+
+sub getArray {
+	# First argument is range of array
+	my $range = @_[0];
+	# Second argument is size of array
+	my $size = @_[1];
+
+	my @tempArray;
+	for (my $i = 0; $i < $size; $i++) {
+		@tempArray[$i] = int(rand($range));
+	}
+
+	return @tempArray;
+}
+
+sub showUnique {
+	my @array = @_;
+
+	print my $last = shift(@array);
+	foreach (@array) {
+		if ($last != $_) {
+			$last = $_;
+			print " ".$_;
+		} 
+	}
+	print "; \n";
+}
+
+sub shellSort {
+	# Argument is array, which we must sort
+	my @array = @_;
+
+	for (my $N = int($#array/2); $N > 0; $N = int($N/2)) {
+		for (my $i = 0; $i <= ($#array-$N); $i++)
+		{
+			while ($i >= 0 && $array[$i]>$array[$i+$N]) {
+				($array[$i],$array[$i+$N]) = ($array[$i+$N],$array[$i]);
+				$i--;
+			}
+		}
+	}
+
+	return @array;
+}
